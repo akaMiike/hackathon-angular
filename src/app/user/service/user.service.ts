18dabe92aca52f95ae3a5f77d63bd6a2a45d17b6
akaMiike <mike.nss@alu.ufc.br>
2023-02-24 02:03:32 -0300

    feat: added initial user service

---
+++ src/app/user/service/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../models/User.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly URL: string = `${environment.api}/users`
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getAllUsers(nameInitial?: string): Observable<User>{
+    return this.http.get<User>(this.URL, {
+      params: {
+        nameInitial: nameInitial
+      }
+    });
+  }
+
+  getUsersBirthdaysOfMonth(month?: string): Observable<User>{
+    return this.http.get<User>(this.URL + '/birthdays', {
+      params:{
+        month: month
+      }
+    })
+  }
+
+  getAllUsersEmailProviders(): Observable<string[]>{
+    return this.http.get<string[]>(this.URL + '/email-providers')
+  }
+
+
+
+}
